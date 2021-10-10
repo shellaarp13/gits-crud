e93eb1e295b873675c6093683dc25258d49f8ff8
shella ardiani <shellaarp13@gmail.com>
2021-10-10 16:55:11 +0000

    Update order_detail_entity.go

--- myapp/entity/order_detail_entity.go
+++ myapp/entity/order_detail_entity.go
@@ -9,15 +9,15 @@ const (
 )
 
 type order_details struct {
-	Order_details_id  	uuid.UUID `gorm:type:uuid;primary_key" json:"order_details"`
-	Order_number      	int64     `gorm:type:integer;not_null" json:"order_number"`
-	Product_id       	uuid.UUID `gorm:type:uuid;not_null" json:"product_id"`
-	Quantity_product 	int64     `gorm:type:integer;not_null" json:"quantity_product"`
+	Order_details_id uuid.UUID `gorm:type:uuid;primary_key" json:"order_details"`
+	Order_number     string    `gorm:type:varchar(50);not_null" json:"order_number"`
+	Product_id       uuid.UUID `gorm:type:uuid;not_null" json:"product_id"`
+	Quantity_product string    `gorm:type:varchar(50);not_null" json:"quantity_product"`
 	Auditable
 }
 
-func NewOrderDetails(order_details_id, product_id uuid.UUID, order_number, quantity_product int) *OrderDetails {
-	return &OrderDetails{
+func NewOrderDetails(order_details_id, product_id uuid.UUID, order_number, quantity_product string) *order_details {
+	return &order_details{
 		Order_details_id: order_details_id,
 		Order_number:     order_number,
 		Product_id:       product_id,
@@ -26,6 +26,6 @@ func NewOrderDetails(order_details_id, product_id uuid.UUID, order_number, quant
 	}
 }
 
-func (model *OrderDetails) TableName() string {
+func (model *order_details) TableName() string {
 	return OrderDetailsTableName
 }
