742bccce707fc49f66d4a01abfd6c1efe7df1f45
shella ardiani <shellaarp13@gmail.com>
2021-10-10 16:55:54 +0000

    Update product_entity.go

--- myapp/entity/product_entity.go
+++ myapp/entity/product_entity.go
@@ -10,18 +10,18 @@ const (
 
 type product struct {
 	Product_ID   uuid.UUID `gorm: "type:uuid;primary_key" json:"product_id"`
-	Stock_P      int64     `gorm: "type:integer;not_null" json:"stok_p"`
+	Stock_P      string    `gorm: "type:varchar(50);not_null" json:"stok_p"`
 	Product_type string    `gorm: "type:varchar(50);not_null" json:"product_type"`
-	Price        int64     `gorm: "type:integer;not_null" json:"price"`
+	Price        string    `gorm: "type:varchar(50);not_null" json:"price"`
 	Auditable
 }
 
-func NewProduct(product_id uuid.UUID, product_type string, stock_p, price int) *product {
+func NewProduct(product_id uuid.UUID, product_type string, stock_p, price string) *product {
 	return &product{
 		Product_ID:   product_id,
-		Stock_P:      int64(stock_p),
+		Stock_P:      stock_p,
 		Product_type: product_type,
-		Price:        int64(price),
+		Price:        price,
 		Auditable:    NewAuditable(),
 	}
 }
