b768ad4a3a45274a820354f4f429ed08c619113f
shella ardiani <shellaarp13@gmail.com>
2021-10-10 13:54:15 +0000

    Update product_entity.go

--- myapp/entity/product_entity.go
+++ myapp/entity/product_entity.go
@@ -10,9 +10,9 @@ const (
 
 type product struct {
 	Product_ID   uuid.UUID `gorm: "type:uuid;primary_key" json:"customer_id"`
-	Stock_P      int64     `gorm: "type:integer;not_null" json:"Stok_P"`
-	Product_type string    `gorm: "type:varchar(50);not_null" json:"Product_type"`
-	Price        int64     `gorm: "type:integer;not_null" json:"Price"`
+	Stock_P      int64     `gorm: "type:integer;not_null" json:"stok_p"`
+	Product_type string    `gorm: "type:varchar(50);not_null" json:"product_type"`
+	Price        int64     `gorm: "type:integer;not_null" json:"price"`
 }
 
 func NewProduct(Product_ID uuid.UUID, Product_type string, Stock_P, Price int) *product {
@@ -26,6 +26,6 @@ func NewProduct(Product_ID uuid.UUID, Product_type string, Stock_P, Price int) *
 }
 
 //Table specifies table name for Product
-func (model *product) TableName() string {
+func (model *Product) TableName() string {
 	return ProductTableName
 }
